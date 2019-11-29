5534a3c59c331750d761886c99cb82bdd9c153a0
zhangjian <zhangjian@hpb.io>
2019-11-29 23:25:17 +0800

    init

---
+++ test/models/article_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class ArticleTest < ActiveSupport::TestCase
+  test "the truth" do
+    assert true
+  end
+  test "should not save article without title" do
+    article = Article.new
+    assert_not article.save, "Saved the article without a title"
+  end
+end
